e5a841560f4f37a3995b10feda2528123c7bac28
errasti <errasti007@yandex.ru>
2022-08-19 20:48:43 +0300

    Initial commit

---
+++ task_002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+void ShowSequence(int start)
+{
+    if ( start <= 0 )
+    {
+        return;
+    }
+    else
+    {
+    Console.Write(start + " ");
+    start--;
+    ShowSequence(start);
+    }
+}   
+ShowSequence(n);
