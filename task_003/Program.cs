35336c943e740a2d749e23e18410a7540c535c44
errasti <errasti007@yandex.ru>
2022-08-19 21:42:37 +0300

    Initial commit

---
+++ task_003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите значение A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение B: ");
+int b = int.Parse(Console.ReadLine());
+
+double Pow(int n, int p)
+{
+    if ( p < 0 )
+    {
+        return 1 / Pow(n,-p);
+    }
+    if (p == 1)
+    {
+        return n;
+    }
+    if (p == 0)
+    {
+        return 1;
+    }
+    return n * Pow(n, --p);
+}
+
+Console.WriteLine(Pow(a,b));
