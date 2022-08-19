4307fcfe94903cace0badbd39538aa558623420c
errasti <errasti007@yandex.ru>
2022-08-19 22:58:16 +0300

    Initial commit

---
+++ hw_03/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+int AkkFunction(int m, int n)
+{
+    if ( m == 0 )
+    {
+        return n + 1;
+    }
+    if ( n == 0 )
+    {
+        return AkkFunction(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return AkkFunction(m - 1, AkkFunction(m, n - 1));
+    }
+    return -1;
+}
+if (AkkFunction(m,n) == -1)
+{
+    Console.WriteLine("Введите неотрицательные M и N ");
+}
+else
+{
+Console.WriteLine("Результат: " + AkkFunction(m,n));
+}
